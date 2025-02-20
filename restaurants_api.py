4156c597a4e7b4cdfa2835e0c15009757a027dce
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2025-02-20 12:58:59 +0000

    Added restaurant data file and imported into the restaurant API file
    - Added export.geojson file to provide raw restaurant data in the london area
    - Imported export.geojson into the restaurant_api file to handle this data

--- restaurants_api.py
+++ restaurants_api.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+import json
+from fastapi import HTTPException
 
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello from the Restaurant API!"}
+try:
+    with open("export.geojson", "r") as f:
+        restaurant_data = json.load(f)
+except Exception as e:
+    restaurant_data = None
+    print("Error loading restaurant data:", e)
