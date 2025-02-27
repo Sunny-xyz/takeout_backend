dd17bc8df0a5a6d838de3196f626698c4b0c71d1
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2025-02-27 12:47:08 +0000

    Added and configured new gems
    - Added the DartSass and govuk_publishing_components gems
    - Mounted the component guide for the govuk_publishing_components gem
    - Adjusted routes to mount component guide
    - Imported supporting sass for components

---
+++ companion_app/app/assets/javascript/application.js
@@ -0,0 +1,4 @@
+# application.js
+//= require govuk_publishing_components/dependencies
+//= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/button
--- companion_app/config/routes.rb
+++ companion_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'recommendations#new'
   resources :recommendations, only: [:new, :create]
+  
+  mount GovukPublishingComponents::Engine, at: "/component-guide" if Rails.env.development?
 end
